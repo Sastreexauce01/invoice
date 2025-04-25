2d84dd056e7047baab1859fe90067ba53043c78c
sastreexauce01 <sastreexauce01@gmail.com>
2025-04-25 14:01:05 +0100

    two commit

---
+++ src/type.ts
@@ -0,0 +1,12 @@
+import { Invoice as PrismaInvoice } from "../generated/prisma"
+import { InvoiceLine } from "../generated/prisma";
+
+export interface Invoice extends PrismaInvoice {
+  lines: InvoiceLine[];
+}
+
+export interface Totals {
+  totalHT: number;
+  totalVAT: number;
+  totalTTC: number;
+}
